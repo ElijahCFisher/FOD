ad0935fb12f41e68294f6f015cc5bc6c68c3be3e
Eli <fishman2028@gmail.com>
2022-02-04 12:52:18 -0700

    picking random file

--- goodshit.js
+++ goodshit.js
@@ -1,7 +1,25 @@
 direc = await window.showDirectoryPicker()
+vals = await direc.values()
+// console.log(Iterators.size(vals))
+var sz = 0
 for await (const entry of direc.values()) {
-    if (entry.kind == 'file')   { 
-        console.log(f=await entry.getFile())
-        console.log(await f.text())
+    if (entry.kind == 'file')   {
+        sz += 1
     }
 }
+i = 0
+ind = Math.floor((Math.random()*sz));
+for await (const entry of direc.values()) {
+    if (entry.kind == 'file')   {
+        if (i == ind) {
+            var f = await entry.getFile()
+            console.log(await f.text())
+            break
+        }
+        i += 1
+    }
+}
+// if (entry.kind == 'file')   { 
+//     console.log(f=await entry.getFile())
+//     console.log(await f.text())
+// }
