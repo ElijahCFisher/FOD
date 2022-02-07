d79b12e39e169f0afec785b75257970ce3e04751
Eli <fishman2028@gmail.com>
2022-02-06 18:09:25 -0700

    fuck await sync loop

--- goodshit.js
+++ goodshit.js
@@ -1,15 +1,27 @@
+function fuck(a) {
+    var ret = []
+    for await (const v of a) {
+        ret.append(v)
+    }
+    return ret
+} 
+
 direc = await window.showDirectoryPicker()
 vals = await direc.values()
 // console.log(Iterators.size(vals))
 var sz = 0
-for await (const entry of direc.values()) {
-    if (entry.kind == 'file')   {
-        sz += 1
-    }
-}
+// var lst = fuck(direc.values())
+// var fileLst = lst.filter(e => e.kind == "file")
+
+// var f = await fileLst[Math.floor((Math.random()*fileLst.length))].getFile()
+// console.log(await f.text())
+
+
+sz = 3
 i = 0
+a = direc.values()
 ind = Math.floor((Math.random()*sz));
-for await (const entry of direc.values()) {
+for await (const entry of a) {
     if (entry.kind == 'file')   {
         if (i == ind) {
             var f = await entry.getFile()
